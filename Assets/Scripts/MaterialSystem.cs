b86b5a98711723aa8d33e277bf58b13f77ae38b7
Estavir-hy <chuaiyukun@foxmail.com>
2026-09-17 21:47:32 +0800

    added farming system as jira said

---
+++ Assets/Scripts/MaterialSystem.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class MaterialSystem : MonoBehaviour, IInteractable
+{
+    [Header("material data")]
+    public float MaterialNum;
+    private float Timer;
+
+    void Start()
+    {
+        MaterialNum = 0;
+        Timer = 0;
+    }
+
+    public void Farm()
+    {
+        if(Timer >= 1)
+        {
+            MaterialNum += 1;
+            Timer = 0;
+        }
+        else
+            Timer += Time.deltaTime;
+    }
+
+    void Update()
+    {
+        Farm();
+        Debug.Log(MaterialNum);
+    }
+
+    public void Interact()
+    {
+        
+    }
+
+}
